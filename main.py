9635fc6054a9b06a14103ff92889ca7c25a2f7f1
Saad Altabari <saadtabari@gmail.com>
2021-09-01 02:11:39 -0700

    fix algorithim to count overall lifeguard coverage

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
 
 	lifeguard_shifts = []
-	with open("files/1.in") as f:
+	with open("files/input/1.in") as f:
 		lifeguard_count = int(f.readline())
 		for lifeguard in range(0, lifeguard_count):
 			start, end = f.readline().split(" ")
@@ -10,27 +10,31 @@ if __name__ == '__main__':
 
 	sorted_lifeguard_shifts = sorted(lifeguard_shifts, key=lambda shift: shift[0])
 	lifeguard_significances = []
-	overall_coverage = 0
 
 	pre_start, pre_end = sorted_lifeguard_shifts.pop(0)
 	significance = pre_end - pre_start
+	overall_coverage = significance
 	while sorted_lifeguard_shifts:
 		start, end = sorted_lifeguard_shifts.pop(0)
 		if end < pre_end:
-			print(0)
-			exit()
+			lifeguard_significances.append(0)
+			continue
 		if start < pre_end:
 			significance = significance - (pre_end - max(start, pre_start))
 			start = pre_end
 
-		overall_coverage += significance
 		lifeguard_significances.append(significance)
 		significance = end - start
+		overall_coverage += significance
 		pre_start = start
 		pre_end = end
 
 	lifeguard_significances.append(significance)
-	overall_coverage += significance
 
 	lifeguard_significances.sort()
 	least_significant = lifeguard_significances.pop(0)
+
+	file1 = open('files/output/1.out', 'w')
+	file1.write(f"{overall_coverage - least_significant}")
+	file1.close()
+	print("execution completed")
